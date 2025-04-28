4638547787c7655f85be4c9e6c8775b55f3bca06
Zuzia <zuzanna.majewskaa@wp.pl>
2025-04-28 23:44:26 +0200

    wip: Add menu

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-from untils.model import users
-from untils.controller import get_user_into
+from utils.model import users
+from utils.controller import get_user_into
 
 def main():
     print(f"Witaj {users[0]["name"]}")
-    get_user_into(users[1:])
+
+    while True:
+        print("============MENU============")
+        print("0 - zamknij aplikacje")
+        print("1 - wyświetl co u znajomych")
+        print("2 - dodaj znajomego użytkownika")
+        print("3 - usuń użytkownika")
+        print("4 - edytuj użytkownika")
+        print("============MENU============")
+
+        choice = input("wybierz opcje menu")
+        if choice == "0":
+            break
+        if choice == "1":
+            get_user_info(users)
+
 
 if __name__ == "__main__":
     main()
