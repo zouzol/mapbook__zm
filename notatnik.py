e0e442dc6b959fe110248cd7e19c4cd65923209c
Zuzia <zuzanna.majewskaa@wp.pl>
2025-04-29 17:21:46 +0200

    wip: prepared remove_user

--- notatnik.py
+++ notatnik.py
@@ -1,12 +1,15 @@
-moja_lista_na_sok = []
-print(moja_lista_na_sok)
+users = [
+    {"name": "Beata", "location": "Lublin", "posts": 555},
+]
 
 
-def add_user(users_data: list) -> None:
-    new_name = input("Podaj imię nowego użytkownika: ")
-    new_location = input("Podaj lokalizacje nowego użytkownika: ")
-    new_posts = int(input("Podaj liczbę postów nowego urzytkownika: "))
-    users_data.append({"name": new_name, "location": new_location, "posts": new_posts})
+def remove_user(users_data: list): ->None
 
-add_user(moja_lista_na_sok)
-print(moja_lista_na_sok)
+    uzytkownik_do_usuniecia = input("Podaj uzytkownika do usuniecia: ")
+
+    for user in users_data:
+
+        if user["name"] == uzytkownik_do_usuniecia:
+            users_data.remove(user)
+
+print(users)
--- utils/controller.py
+++ utils/controller.py
@@ -1,27 +1,18 @@
 def get_user_info(users_data: list) -> None:
     for user in users_data:
-        print(f"Twój znajomy {user["name"]} z {user["location"]} opublikował {user["posts"]} postów.")
-
+        print(f"Twój znajomy {user['name']} z miejscowości: {user['location']}, opublikował {user['posts']} postów ")
 
 def add_user(users_data: list) -> None:
-    new_name = input("Podaj imię nowego użytkownika: ")
-    new_location = input("Podaj lokalizacje nowego użytkownika: ")
-    new_posts = int(input("Podaj liczbę postów nowego urzytkownika: "))
-    users_data.append({"name": new_name, "location": new_location, "posts": new_posts})
+    new_name = input('Podaj imię nowego użytkownika: ')
+    new_location = input('Podaj lokalizację nowego użytkownika: ')
+    new_posts = int(input('Podaj liczbę postów nowego użytkownika: '))
+    users_data.append({'name': new_name , 'location': new_location, 'posts': new_posts})
 
+def remove_user(users_data: list) -> None:
+    użytkownik_do_usunięcia = input('Podaj użytkownika do usunięcia: ')
 
-users = [
-    {"name": "Beata", "location": "Lublin", "posts": 555},
-]
+    for user in users_data:
 
+        if user['name'] == użytkownik_do_usunięcia:
+            users_data.remove(user)
 
-def remove_user(users_data: list): ->None
-
-uzytkownik_do_usuniecia = input("Podaj uzytkownika do usuniecia: ")
-
-for user in users_data:
-
-if user["name"] == uzytkownik_do_usuniecia:
-    users_data.remove(user)
-
-print(users)
