e9b0f5a7b2fbc445e495ec8a7b98dabb5f813653
Zuzia <zuzanna.majewskaa@wp.pl>
2025-04-29 16:44:49 +0200

    wip: prepared add_user

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
+from utils.controller import get_user_info, add_user
 from utils.model import users
-from utils.controller import get_user_into, add_user
+
 
 def main():
-    print(f"Witaj {users[0]["name"]}")
-
     while True:
-        print("============MENU============")
-        print("0 - zamknij aplikacje")
+        print("=============MENU=============")
+        print("0 - zamknij aplikację")
         print("1 - wyświetl co u znajomych")
-        print("2 - dodaj znajomego użytkownika")
-        print("3 - usuń użytkownika")
+        print("2 - dodaj nowego użytkownika")
+        print("3 - usuń nowego użytkownika")
         print("4 - edytuj użytkownika")
-        print("============MENU============")
+        print("=============MENU=============")
 
-        choice = input("wybierz opcje menu")
-        if choice == "0": break
-        if choice == "1": get_user_info(users)
+        choice = input("Wybierz opcję menu ")
+        if choice == "0":
+            break
+        if choice == "1":
+            get_user_info(users)
         if choice == "2": add_user(users)
-
 
 
 if __name__ == "__main__":
     main()
-
-
 
--- utils/controller.py
+++ utils/controller.py
@@ -1,9 +1,10 @@
-def get_user_into(user_data:list)->None:
+def get_user_into(user_data: list) -> None:
     for user in users:
         print(f"Twój znajomy {user["name"]} z {user["location"]} opublikował {user["posts"]} postów.")
 
-def add_user(users_data:list):
-    new_name=input("Podaj imię:")
-    new_location=input("Podaj miejsce: ")
-    new_posts=input("Podaj liczbę postów: ")
-    users_data.append({"name":new_name,"location":new_location,"posts":new_posts})
+
+def add_user(users_data: list) -> None:
+    new_name = input("Podaj imię nowego użytkownika: ")
+    new_location = input("Podaj lokalizacje nowego użytkownika: ")
+    new_posts = int(input("Podaj liczbę postów nowego urzytkownika: "))
+    users_data.append({"name": new_name, "location": new_location, "posts": new_posts})
