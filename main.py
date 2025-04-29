88cd8e82e5db933df9de21b36e4be8deab256fdb
Zuzia <zuzanna.majewskaa@wp.pl>
2025-04-29 17:14:46 +0200

    wip: prepared add_user

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from utils.controller import get_user_info, add_user
+from utils.controller import get_user_info, add_user, remove_user
 from utils.model import users
 
 
@@ -13,13 +13,11 @@ def main():
         print("=============MENU=============")
 
         choice = input("Wybierz opcję menu ")
-        if choice == "0":
-            break
-        if choice == "1":
-            get_user_info(users)
+        if choice == "0": break
+        if choice == "1": get_user_info(users)
         if choice == "2": add_user(users)
+        if choice == "3": remove_user(users)
 
 
 if __name__ == "__main__":
     main()
-
--- utils/controller.py
+++ utils/controller.py
@@ -8,3 +8,20 @@ def add_user(users_data: list) -> None:
     new_location = input("Podaj lokalizacje nowego użytkownika: ")
     new_posts = int(input("Podaj liczbę postów nowego urzytkownika: "))
     users_data.append({"name": new_name, "location": new_location, "posts": new_posts})
+
+
+users = [
+    {"name": "Beata", "location": "Lublin", "posts": 555},
+]
+
+
+def remove_user(users_data: list): ->None
+
+uzytkownik_do_usuniecia = input("Podaj uzytkownika do usuniecia: ")
+
+for user in users_data:
+
+if user["name"] == uzytkownik_do_usuniecia:
+    users_data.remove(user)
+
+print(users)
